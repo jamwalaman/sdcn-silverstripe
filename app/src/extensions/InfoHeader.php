a74e444ea9d6a176d1b80e4fa1a39e7eb1823903
Aman Jamwal <aman.jamwal95@gmail.com>
2022-12-25 13:13:15 +1300

    SiteConfig. Add InfoHeader

---
+++ app/src/extensions/InfoHeader.php
@@ -0,0 +1,19 @@
+<?php
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\ORM\DataExtension;
+
+class InfoHeader extends DataExtension 
+{
+    
+    private static $db = [
+        'HeaderContent' => 'HTMLText'
+    ];
+
+    public function updateCMSFields(FieldList $fields) 
+    {
+        $fields->addFieldToTab("Root.Main", 
+            new HTMLEditorField("HeaderContent", "Header Info Content")
+        );
+    }
+}
